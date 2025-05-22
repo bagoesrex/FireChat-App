5e4074909127bf73e698704a73e080ef693ae6c7
bagoesrex <bagoesrex@gmail.com>
2025-05-22 12:31:40 +0700

    feat: display chat messages from firestore in real time

--- lib/widgets/chat_messages.dart
+++ lib/widgets/chat_messages.dart
@@ -1,10 +1,39 @@
 import 'package:flutter/material.dart';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class ChatMessages extends StatelessWidget {
   const ChatMessages({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('Tidak ada pesan.'),);
+    return StreamBuilder(
+      stream:
+          FirebaseFirestore.instance
+              .collection('chat')
+              .orderBy('createdAt', descending: false)
+              .snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        }
+
+        if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+          return const Center(child: Text('Tidak ada pesan.'));
+        }
+
+        if (snapshot.hasError) {
+          return const Center(child: Text('Gagal memuat pesan.'));
+        }
+
+        final loadedMessages = snapshot.data!.docs;
+
+        return ListView.builder(
+          itemCount: loadedMessages.length,
+          itemBuilder:
+              (context, index) => Text(loadedMessages[index].data()['text']),
+        );
+      },
+    );
   }
 }
